b79031811e2ac5b0aebb6fe0412501d2e8d92ca0
nanato-okajima <minto328n@gmail.com>
2025-11-25 21:52:57 +0900

    change: attendance

--- database/attendance.go
+++ database/attendance.go
@@ -12,24 +12,24 @@ func NewAttendanceDatabase() repository.AttendanceRepository {
 }
 
 func (ad attendanceDatabase) Insert(attendance *repository.Attendance) error {
-	result := DB.Client.Create(attendance)
+	result := DB.Create(attendance)
 
 	return result.Error
 }
 
 func (ad attendanceDatabase) Fetch(attendances *[]repository.Attendance) error {
-	result := DB.Client.Find(&attendances)
+	result := DB.Find(&attendances)
 
 	return result.Error
 }
 
 func (ad attendanceDatabase) Update(attendance *repository.Attendance, id string) error {
-	result := DB.Client.Model(&repository.Attendance{}).Where("attendance_id = " + id).Updates(attendance)
+	result := DB.Model(&repository.Attendance{}).Where("attendance_id = ?", id).Updates(attendance)
 
 	return result.Error
 }
 
 func (ad attendanceDatabase) Delete(id string) error {
-	result := DB.Client.Where("attendance_id = " + id).Delete(&repository.Attendance{})
+	result := DB.Where("attendance_id = ?", id).Delete(&repository.Attendance{})
 	return result.Error
 }
