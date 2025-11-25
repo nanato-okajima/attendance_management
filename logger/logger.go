3e29155e3d9ec7a7e3415f4865dad704a0e18aec
nanato-okajima <minto328n@gmail.com>
2025-11-25 21:51:58 +0900

    chabge: logger更新

--- logger/logger.go
+++ logger/logger.go
@@ -2,11 +2,45 @@ package logger
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Logger *zap.SugaredLogger
 
-func SetupLogger() {
-	logger, _ := zap.NewDevelopment()
+func SetupLogger() error {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	logger, err := config.Build()
+	if err != nil {
+		return err
+	}
+
 	Logger = logger.Sugar()
+	return nil
+}
+
+func Info(args ...interface{}) {
+	Logger.Info(args...)
+}
+
+func Infof(template string, args ...interface{}) {
+	Logger.Infof(template, args...)
+}
+
+func Error(args ...interface{}) {
+	Logger.Error(args...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	Logger.Errorf(template, args...)
+}
+
+func Warn(args ...interface{}) {
+	Logger.Warn(args...)
+}
+
+func Warnf(template string, args ...interface{}) {
+	Logger.Warnf(template, args...)
 }
