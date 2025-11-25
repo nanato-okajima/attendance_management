8f9e448e576beac9609e6ee50586456f91512b0e
nanato-okajima <minto328n@gmail.com>
2025-11-25 21:52:09 +0900

    chabge: myerrors更新

--- myerrors/myerrors.go
+++ myerrors/myerrors.go
@@ -1,21 +1,51 @@
 package myerrors
 
-type AppError interface {
-	error
-}
+import "fmt"
 
 type BadRequestError struct {
 	Err error
+}
+
+func (e *BadRequestError) Error() string {
+	return fmt.Sprintf("bad request: %v", e.Err)
+}
+
+type UnauthorizedError struct {
+	Err error
+}
+
+func (e *UnauthorizedError) Error() string {
+	return fmt.Sprintf("unauthorized: %v", e.Err)
+}
+
+type ForbiddenError struct {
+	Err error
+}
+
+func (e *ForbiddenError) Error() string {
+	return fmt.Sprintf("forbidden: %v", e.Err)
+}
+
+type NotFoundError struct {
+	Err error
+}
+
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("not found: %v", e.Err)
 }
 
 type InternalServerError struct {
 	Err error
 }
 
-func (br *BadRequestError) Error() string {
-	return br.Err.Error()
+func (e *InternalServerError) Error() string {
+	return fmt.Sprintf("internal server error: %v", e.Err)
 }
 
-func (br *InternalServerError) Error() string {
-	return br.Err.Error()
+type ConflictError struct {
+	Err error
+}
+
+func (e *ConflictError) Error() string {
+	return fmt.Sprintf("conflict: %v", e.Err)
 }
